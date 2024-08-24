d6576140978bdc10340578dae747db194fffac14
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-08-24 12:12:42 +0300

    add necessary dependencies | add files

--- src/App.js
+++ src/App.js
@@ -1,86 +1,15 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Homepage from "./Pages/Website/Homepage";
+import Login from "./Pages/Website/Login";
 
 function App() {
-  // Set up form data
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-    message: "",
-  });
-
-  // Handle form change
-
-  function handleFormChange(e) {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
-  }
-
-  console.log(form);
-
-  // Render Data
   return (
-    <div className="App ">
-      <div className="container">
-        <div className="mb-3">
-          <label htmlFor="exampleFormControlInput1" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            name="email"
-            className="form-control"
-            id="exampleFormControlInput1"
-            placeholder="name@example.com"
-            value={form.email}
-            onChange={handleFormChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="pass" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            className="form-control"
-            id="pass"
-            placeholder="password..."
-            value={form.password}
-            onChange={handleFormChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="confirmPass" className="form-label">
-            Confirm Password
-          </label>
-          <input
-            type="password"
-            name="confirmPassword"
-            className="form-control"
-            id="confirmPass"
-            placeholder="confirm password..."
-            value={form.confirmPassword}
-            onChange={handleFormChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleFormControlTextarea1" className="form-label">
-            Message
-          </label>
-          <textarea
-            className="form-control"
-            name="message"
-            id="exampleFormControlTextarea1"
-            rows={3}
-            value={form.message}
-            onChange={handleFormChange}
-          />
-        </div>
-      </div>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
