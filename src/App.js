cc6baa3c145d2f71729cc5571d94c8c99b5e4d00
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-08-24 08:52:31 +0300

    merge many vars in one useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,88 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App ">test</div>;
+  // Set up form data
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    message: "",
+  });
+
+  // Handle form change
+
+  function handleFormChange(e) {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  console.log(form);
+
+  // Render Data
+  return (
+    <div className="App ">
+      <div className="container">
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            name="email"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+            value={form.email}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="pass" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            className="form-control"
+            id="pass"
+            placeholder="password..."
+            value={form.password}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="confirmPass" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            name="confirmPassword"
+            className="form-control"
+            id="confirmPass"
+            placeholder="confirm password..."
+            value={form.confirmPassword}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">
+            Message
+          </label>
+          <textarea
+            className="form-control"
+            name="message"
+            id="exampleFormControlTextarea1"
+            rows={3}
+            value={form.message}
+            onChange={handleFormChange}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
