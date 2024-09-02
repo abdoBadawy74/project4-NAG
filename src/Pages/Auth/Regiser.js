bb56ca730dcaa870b3810ebd1ca76e9a70c381a0
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-09-02 11:19:48 +0300

    edit style of form

--- src/Pages/Auth/Regiser.js
+++ src/Pages/Auth/Regiser.js
@@ -34,52 +34,61 @@ export default function Regiser() {
     }
   }
   return (
-    <div onSubmit={handleFormSubmit} className="container">
-      <form className="w-75 m-auto mt-5">
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Name:
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="form-control"
-            placeholder="Enter your Name.."
-            value={form.name}
-            onChange={handleFormChange}
-          />
-        </div>
+    <div className="container">
+      <div className="row h-100">
+        <form className="form " onSubmit={handleFormSubmit}>
+          <h1 className="text-center">Register Now</h1>
+          <div className="custom-form">
+            <div className="form-control">
+              <input
+                type="text"
+                id="name"
+                className="form-control"
+                placeholder="Enter your Name.."
+                value={form.name}
+                onChange={handleFormChange}
+                required
+              />
+              <label htmlFor="name" className="form-label">
+                Name
+              </label>
+            </div>
 
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email:
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="form-control"
-            placeholder="Enter your Email.."
-            value={form.email}
-            onChange={handleFormChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password:
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="form-control"
-            placeholder="Enter your password.."
-            value={form.password}
-            onChange={handleFormChange}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary w-100">
-          Register
-        </button>
-      </form>
+            <div className="form-control">
+              <input
+                type="email"
+                id="email"
+                className="form-control"
+                placeholder="Enter your Email.."
+                value={form.email}
+                onChange={handleFormChange}
+                required
+              />
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+            </div>
+            <div className="form-control">
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                placeholder="Enter your password.."
+                value={form.password}
+                onChange={handleFormChange}
+                required
+                minLength={6}
+              />
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+            </div>
+            <button type="submit" className="btn btn-primary w-100">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./Css/Components/Form.css"
 import "./Css/Base/media.css"
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
