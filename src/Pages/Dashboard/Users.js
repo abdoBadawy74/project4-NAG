a2808e1bf2f05ed69e0107f25461a97e375872be
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-10-16 10:01:25 +0300

    show users in the table

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,25 +1,59 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BaseURL, USERS } from "../../Api/Api";
 import Cookie from "cookie-universal";
+import { Table } from "react-bootstrap";
 
 export default function Users() {
-    const cookie = Cookie();
+  const [users, setUsers] = useState([]);
+  const cookie = Cookie();
   useEffect(() => {
     axios
-      .get(`${BaseURL}/${USERS}`,{
+      .get(`${BaseURL}/${USERS}`, {
         headers: {
           Authorization: `Bearer ${cookie.get("e-commerce")}`,
         },
       })
       .then((res) => {
         console.log(res);
+        setUsers(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
-  return <>
-  <div>Users page</div>
-  </>;
+
+  return (
+    <div className="bg-white w-100 p-2">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr
+              key={user.id}
+              className="p-0"
+              style={{
+                lineHeight: "2.5",
+              }}
+            >
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>
+                <button className="btn btn-danger">Delete</button>
+                <button className="btn btn-warning">Edit</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
