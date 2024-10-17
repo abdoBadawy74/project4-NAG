f78fc8bb55cfdd8106b40161c9ed5353815f4e20
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-10-17 12:56:33 +0300

    add user from dashboard

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import GoogleCallBack from "./Pages/Auth/GoogleCallBack";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import RequireAuth from "./Pages/Auth/RequireAuth";
 import User from "./Pages/Dashboard/User";
+import AddUser from "./Pages/Dashboard/AddUser";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="dashboard/*" element={<Dashboard />}>
             <Route path="users" element={<Users />} />
             <Route path="users/:id" element={<User />} />
+            <Route path="users/add" element={<AddUser />} />
           </Route>
         </Route>
       </Routes>
---
+++ src/Pages/Dashboard/AddUser.js
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import { Form } from "react-bootstrap";
+import { Axios } from "../../Api/axios";
+import { USER } from "../../Api/Api";
+import Loading from "../../Components/Loading/Loading";
+
+export default function AddUser() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  //   function handleSubmit
+  async function handleSubmit(e) {
+    setLoading(true);
+    e.preventDefault();
+    try {
+      const res = await Axios.post(`${USER}/add`, {
+        name,
+        email,
+        password,
+        role,
+      });
+      console.log(res);
+      window.location.replace("/dashboard/users");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  //   console.log(name);
+  //   console.log(email);
+
+  return (
+    <>
+      {loading && <Loading />}
+      <Form className="bg-white w-100 mx-2 p-3" onSubmit={handleSubmit}>
+        <h2 className="my-3">Update User</h2>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>User Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="name..."
+            value={name}
+            onChange={(e) => setName(() => e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email.."
+            value={email}
+            onChange={(e) => setEmail(() => e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password.."
+            required
+            value={password}
+            onChange={(e) => setPassword(() => e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+          <Form.Label>Role</Form.Label>
+          <Form.Select
+            value={role}
+            onChange={(e) => setRole(() => e.target.value)}
+          >
+            <option disabled value={""}>
+              Select Role
+            </option>
+            <option value="1995">Admin</option>
+            <option value="2001">User</option>
+            <option value="1996">Writer</option>
+          </Form.Select>
+        </Form.Group>
+
+        <button
+          className="btn btn-primary"
+          disabled={
+            name === "" || email === "" || role === "" || password === ""
+              ? true
+              : false
+          }
+        >
+          Save Edits
+        </button>
+      </Form>
+    </>
+  );
+}
