273fea2718d6be3c6fab2efc695480cd4664de22
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-11-27 00:51:46 +0200

    fixing logic error in helper function

--- src/Helpers/TitleSlice.js
+++ src/Helpers/TitleSlice.js
@@ -1,5 +1,6 @@
 export default function TitleSlice(title, start, end) {
-  const slicedTitle = title.slice(start, end) + "...";
+  const slicedTitle =
+    title.length > end ? title.slice(start, end) + "..." : title;
 
   return slicedTitle;
 }
