e6b3e5cdfbcf6a6df0345e434cd56dffc20dcd21
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-12-01 10:44:26 +0200

    get product info and add images to slider

--- src/Pages/Website/SingleProduct/SingleProduct.js
+++ src/Pages/Website/SingleProduct/SingleProduct.js
@@ -1,22 +1,39 @@
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
 import ImageGallery from "react-image-gallery";
-
+import { useParams } from "react-router-dom";
+import { Axios } from "../../../Api/axios";
+import { PRODUCT } from "../../../Api/Api";
 
 export default function SingleProduct() {
-    const images = [
-      {
-        original: "https://picsum.photos/id/1018/1000/600/",
-        thumbnail: "https://picsum.photos/id/1018/250/150/",
-      },
-      {
-        original: "https://picsum.photos/id/1015/1000/600/",
-        thumbnail: "https://picsum.photos/id/1015/250/150/",
-      },
-      {
-        original: "https://picsum.photos/id/1019/1000/600/",
-        thumbnail: "https://picsum.photos/id/1019/250/150/",
-      },
-    ];
+  const [product, setProduct] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    Axios.get(`${PRODUCT}/${id}`)
+      .then((res) => {
+        console.log(res);
+        setProduct(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const images = product[0]?.images.map((image) => ({
+    original: image.image,
+    thumbnail: image.image,
+  }));
+
+  console.log(product[0]?.images);
+
   return (
-    <ImageGallery items={images} />
-  )
+    <Container className="mt-5">
+      <div className="d-flex align-ite justify-content-between flex-wrap">
+        <div className="col-lg-4 col-md-6 col-12">
+          <ImageGallery items={images} />
+        </div>
+      </div>
+    </Container>
+  );
 }
